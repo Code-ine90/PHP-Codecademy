42b9118f0193a866465133b8b16d0e927e0b5774
Stuart White <srm.white@yahoo.co.uk>
2020-05-10 16:24:50 +0100

    End Session 3

--- Session-3.php
+++ Session-3.php
@@ -7,7 +7,7 @@
     function inflateEgo() 
     {
         echo "You are awesome.";
-    }
+    };
 
     # We can then call the function by using it's name followed by parenthesis, this tells the computer to grab the instructions specified in the function definition and execute them.
 
@@ -21,7 +21,7 @@
     {
         echo "My favourite number is: "; // This will print when the function is called.
         return 112; // This will be returned when the function is called.
-    }
+    };
 
     $my_num = printStringReturnNumber(); // This will call the function and assign it's return value to the variable $my_num. So it should print "My favourite number is: ", and return the value 112 to the variable.
 
@@ -32,17 +32,17 @@
     function first()
     {
     return "You did it!\n";
-    }
+    };
 
     function second()
     {
     return "You're amazing!\n";
-    }
+    };
 
     function third()
     {
     return "You're a coding hero!\n";
-    }
+    };
 
     echo first() . second() . third(); // Should output You did it! You're amazing! You're a coding hero! over 3 seperate lines.
 
@@ -53,7 +53,7 @@
     function returnNothing() 
     {
     echo "I'm running! I'm running!\n";
-    }
+    };
 
     $result = returnNothing(); // Prints: I'm running! I'm running!
 
@@ -66,7 +66,7 @@
     function increaseEnthusiasm($string)
     {
     return $string . "!\n"; // Returns the argument put in, with an exclamation appended on the end.
-    }
+    };
 
     echo increaseEnthusiasm("I'm awesome"); // Prints "I'm awesome!"
     echo increaseEnthusiasm("And cool"); // Prints "And cool!"
@@ -74,7 +74,7 @@
     function repeatThreeTimes($string)
     {
     return $string . $string . $string; // This should return the arguement input three times in a row.
-    }
+    };
 
     echo repeatThreeTimes("Hello\n"); // Should print "HelloHelloHello".
 
@@ -88,17 +88,100 @@
     function calculateArea($num1, $num2) 
     {
     return $num1 * $num2;
-    }
+    };
 
     echo calculateArea(5, 10) . "\n";
 
     function calculateVolume($num1, $num2, $num3) 
     {
     return $num1 * $num2 * $num3;
-    }
+    };
 
     echo calculateVolume(5, 10, 4) . "\n";
 
     echo "The total volume of concrete to be ordered for pad 2 is: " . calculateVolume(4.2, 16, .15)  . " cubic meters";
+
+    # We can also add a default value to a parameter. In this example, we want to calculate the tip left for a meal, but if the person wants to just use the standard 20% tip, they can leave the input blank.
+
+    function calculateTip($mealCost, $tip = 20)
+    {
+    return $mealCost * (1 + ($tip/100));
+    };
+
+    echo calculateTip(100, 10) . "\n";
+    echo calculateTip(100) . "\n";
+    echo calculateTip(65, 15) . "\n";
+
+    # Much the same as with variables, when we assign a variable as the argument to be passed to the function, it will not make permanent changes to the variable unless we use the reference sign (&) and make the parameter an alias for the variable.
+
+    function addX ($param)
+    {
+    $param = $param . "X";
+    echo $param;
+    };
+    $word = "Hello";
+    addX($word); // Prints: HelloX
+    echo $word; // Prints: Hello
+
+    # If we prepend the parameter with the reference sign (&) we will end up with a differt output though.
+
+    function addXPermanently (&$param)
+    {
+        $param = $param . "X";
+        echo $param;
+    };
+
+    $word = "Hello";
+    addXPermanently($word); // Prints "HelloX"
+    echo $word; // Also prints "HelloX"
+
+    # Variable Scope
+
+    # Parameters defined for a function have a local scope, meaning that can only be used within that function.
+
+    function calculateDaysLeft($feed_quantity, $number, $rate)
+    {
+    $result = $feed_quantity / ($number * $rate);
+    return $result;
+    }
+    echo calculateDaysLeft(300, 2, 30);
+
+    # If we tried to echo $result outside of the function, it would give us an error (Undefined variable). 
+
+    # However many variables can rely on the same piece of data, and this may be stored in a global variable. To access a global variable in a function we have to use the "global" keyword to tell PHP to look in the global scope for the variable instead of just the local scope.
+
+    $feed_quantity = 300;
+    function calculateDaysLeft($number, $rate)
+    {
+    global $feed_quantity;
+    $result = $feed_quantity / ($number * $rate);
+    return $result;
+    }
+    echo calculateDaysLeft(2, 120);
+
+    # In this next example we have been asked to change the function so that we can use the global variable $language instead of filling out the function parameters each time with the same information.
+
+    $language = "PHP";
+    $topic = "scope";
+
+    function generateLessonName($language, $concept)
+    {
+    return $language . ": " . $concept;
+    }
+
+    echo generateLessonName($language, $topic);
+
+    # This is what it looks like afterwards.
+
+    function generateLessonName($concept)
+    {
+    global $language;
+    return $language . ": " . $concept;
+    }
+
+    echo generateLessonName($topic); // Should print "PHP: scope"
+    echo generateLessonName("parameter"); // Should print "PHP: parameters"
+
+    
 
 ?>
